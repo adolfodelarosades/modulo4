4d4ff7f73ae5131ffa11f29b9ad2f24de5f8cbef
mongogetafe2020 <mongogetafe2020@gmail.com>
2020-02-10 16:49:44 +0100

    ct

--- validators.js
+++ validators.js
@@ -40,3 +40,48 @@ db.createCollection(
         }
     }
 )
+
+
+db.runCommand({
+    collMod: "clientes",
+    validator: {
+        $jsonSchema: {
+            bsonType: "object",
+            required: ["nombre", "apellidos", "fechaNac", "direccion"],
+            properties: {
+                nombre: {
+                    bsonType: "string",
+                    description: "debe ser un string y es obligatorio" 
+                },
+                apellidos: {
+                    bsonType: "string",
+                    description: "debe ser un string y es obligatorio" 
+                },
+                fechaNac: {
+                    bsonType: "date",
+                    description: "debe ser un date y es obligatorio" 
+                },
+                direccion: {
+                    bsonType: "object",
+                    required: ["calle","cp","localidad"],
+                    properties: {
+                        calle: {
+                            bsonType: "string",
+                            description: "debe ser un string y es obligatorio"
+                        },
+                        cp: {
+                            bsonType: "string",
+                            description: "debe ser un string y es obligatorio"
+                        },
+                        localidad: {
+                            bsonType: "string",
+                            description: "debe ser un string y es obligatorio"
+                        }
+                    }
+                }
+            }
+        }
+    },
+    validationLevel: "moderate",
+    validationAction: "warn"
+})
